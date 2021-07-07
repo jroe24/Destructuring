d5ced8b0d1c31c523780bdb85c18e97faf89e6af
Jacob Roe <jroenavy24@gmail.com>
2021-07-07 14:37:37 -0700

    Changed 1 answer

--- destructuring.js
+++ destructuring.js
@@ -54,4 +54,4 @@ const [ ,willThisWork] = secondKey;
 console.log(key); // value
 console.log(secondKey); // [1,5,1,8,3,3]
 console.log(secondKey[0]); // 1
-console.log(willThisWork); // Error: Can't have the ',' in const
+console.log(willThisWork); // 5
